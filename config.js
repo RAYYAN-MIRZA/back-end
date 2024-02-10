80a6f2e94e15f4eae417c627693c59c4dd6dd542
RAYYAN-MIRZA <mirzarayyan125@gmail.com>
2024-02-11 04:12:02 +0500

    initial commit

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 export const Port=3001;
 
 export const mongoUrl=
-"mongodb+srv://mirzarayyan125:shitShitshit123@cluster0.eih7vxi.mongodb.net/test_db?retryWrites=true&w=majority"
+"mongodb+srv://mirzarayyan125:PASSWORD@cluster0.eih7vxi.mongodb.net/test_db?retryWrites=true&w=majority"
